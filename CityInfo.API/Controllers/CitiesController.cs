3c1d6a9b0bea9c912f4ad909af0b48cccf0d8004
Issaki <nouri.issaki@capgemini.com>
2022-08-19 10:10:45 +0200

    Added versioning to our API

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -8,8 +8,9 @@ using System.Text.Json;
 namespace CityInfo.API.Controllers
 {
     [ApiController]
-    [Authorize]
-    [Route("api/cities")]
+    //[Authorize]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/cities")]
     public class CitiesController : ControllerBase
     {       
         private readonly ICityInfoRepository _cityInfoRepository;
